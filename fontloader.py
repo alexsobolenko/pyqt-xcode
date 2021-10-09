015cb56e0e9291040ca35196fb7d1ac191cdc14d
Alexey Sobolenko <alexsobolenko@gmail.com>
2021-10-09 22:11:27 +0300

    fix

--- fontloader.py
+++ fontloader.py
@@ -1,16 +1,17 @@
 from PyQt5.QtCore import QIODevice, QByteArray, QFile
 from PyQt5.QtGui import QFontDatabase, QFont
 
-class FontLoader(QFont):
-	def __init__(self, parent, fileName, pointSize):
-		super().__init__()
 
-		file = QFile(fileName + '.ttf')
-		file.open(QIODevice.ReadOnly)
-		fontData = QByteArray(file.readAll())
-		dbFont = QFontDatabase()
-		idFont = dbFont.addApplicationFontFromData(fontData)
-		fontFamily = dbFont.applicationFontFamilies(idFont)[0]
-		#font = QFont(fontFamily)
-		self.setFamily(fontFamily)
-		self.setPointSize(pointSize)
+class FontLoader(QFont):
+    def __init__(self, parent, fileName, pointSize):
+        super().__init__()
+
+        file = QFile(fileName + '.ttf')
+        file.open(QIODevice.ReadOnly)
+        fontData = QByteArray(file.readAll())
+        dbFont = QFontDatabase()
+        idFont = dbFont.addApplicationFontFromData(fontData)
+        fontFamily = dbFont.applicationFontFamilies(idFont)[0]
+        # font = QFont(fontFamily)
+        self.setFamily(fontFamily)
+        self.setPointSize(pointSize)
